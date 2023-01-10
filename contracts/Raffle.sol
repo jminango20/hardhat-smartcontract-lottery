e0c3c9db75b12810014342952a0e0958b3b64b87
jminango20 <jcarlosminango@gmail.com>
2023-01-10 10:20:00 -0300

    raffle.sol setup

---
+++ contracts/Raffle.sol
@@ -0,0 +1,13 @@
+// Raffle
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes -> completly automate
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keeper)
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Raffle{
+    
+}
