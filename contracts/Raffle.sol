f0c692ec36f905192d157a7a7e9e3826f9923cf5
jminango20 <jcarlosminango@gmail.com>
2023-01-21 16:29:01 -0300

    implementing chainlink VRF

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,9 +8,11 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle{
+contract Raffle is VRFConsumerBaseV2{
 
     /*State Variables*/
     uint256 private immutable i_entranceFee;
@@ -19,7 +21,7 @@ contract Raffle{
     /*Events*/
     event raffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee){
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
         i_entranceFee = entranceFee;
     }
 
@@ -34,6 +36,18 @@ contract Raffle{
         emit raffleEnter(msg.sender);
     }
 
+    function requestRandomWinner() external{
+        //Request the random number
+        //Once we get it, do something with it
+        //2 transaction process
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randowmWords) 
+    internal 
+    override
+    {}
+
+    /* View / Pure functions */
     function getEntranceFee() public view returns (uint256){
         return i_entranceFee;
     }
