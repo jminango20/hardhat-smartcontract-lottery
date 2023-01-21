77212a83b56acbf10fe2cefcbfe8500ab7a0e3fc
jminango20 <jcarlosminango@gmail.com>
2023-01-21 17:10:14 -0300

    Implementing Chainlink VRF - The Request

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,6 +9,8 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 
 error Raffle__NotEnoughETHEntered();
 
@@ -17,12 +19,32 @@ contract Raffle is VRFConsumerBaseV2{
     /*State Variables*/
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     /*Events*/
-    event raffleEnter(address indexed player);
+    event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit
+        ) VRFConsumerBaseV2(vrfCoordinatorV2){
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+       
+        i_subscriptionId = subscriptionId;
         i_entranceFee = entranceFee;
+       
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable{
@@ -33,13 +55,21 @@ contract Raffle is VRFConsumerBaseV2{
         s_players.push(payable(msg.sender)); 
         // Emit an event when we update a dynamic array or mapping
         // Named events with the function name reversed
-        emit raffleEnter(msg.sender);
+        emit RaffleEnter(msg.sender);
     }
 
     function requestRandomWinner() external{
         //Request the random number
         //Once we get it, do something with it
         //2 transaction process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, //gasLane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randowmWords) 
